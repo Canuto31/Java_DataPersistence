c6fbdc63476b429757172b6a9e6ca6a38a35fced
Juan Moya <jcmt.personal.vg@gmail.com>
2023-05-24 20:23:51 -0500

    Mysql Connection

--- MensajesApp/src/main/java/org/example/Main.java
+++ MensajesApp/src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Conection conection = new Conection();
+
+        try (Connection cnx = conection.get_connection()) {
+
+        } catch (Exception e) {
+
+        }
     }
 }
